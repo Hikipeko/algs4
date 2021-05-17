4dc4348caccab0144d7cc5bcfc032c92038775fe
Hikipeko <1979160133@qq.com>
2021-05-17 12:12:14 +0800

    push 2

--- chapter1/VisualAccumulator.java
+++ chapter1/VisualAccumulator.java
@@ -1,3 +1,7 @@
+import edu.princeton.cs.algs4.StdDraw;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
 public class VisualAccumulator {
     private double total;
     private int N;
